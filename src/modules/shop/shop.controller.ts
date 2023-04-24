611cf0f15b6e67278a6b378ed000931f41da696a
Hai Nguyen <hainguyen27798@gmail.com>
2023-04-24 22:37:59 +0700

    hash password and get shop

--- src/modules/shop/shop.controller.ts
+++ src/modules/shop/shop.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 import { AccountDto } from '@dto/account.dto';
 import { ShopService } from '@modules/shop/shop.service';
 
@@ -9,5 +9,10 @@ export class ShopController {
     @Post('signup')
     signup(@Body() account: AccountDto) {
         return this._ShopService.createShop(account);
+    }
+
+    @Get('')
+    findAll() {
+        return this._ShopService.findAll();
     }
 }
